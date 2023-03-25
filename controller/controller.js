3478f0cf61407fbd3a3fe51cb105b867ab99d1a0
Shubham <sanglehsubham9@gmail.com>
2023-03-25 23:05:11 +0530

    taking PGP Message as parameter

--- controller/controller.js
+++ controller/controller.js
@@ -1,55 +1,46 @@
-import openpgp from 'openpgp'
+import openpgp from "openpgp";
 
-
-let armoredPrivateKey , armoredPublicKey
+let armoredPrivateKey, armoredPublicKey;
 export async function generateKeyPair(data) {
-    let {privateKey , publicKey} =  await openpgp.generateKey({
-        curve: 'ed25519',
-        userIDs: [
-            {
-                name: 'Shubham Sangle', email: 'sangleshubham9@gmail.com',
-                comment: 'This key is for public sharing'
-            }
-        ],
-        passphrase: 'secret',
-    });
+  let { privateKey, publicKey } = await openpgp.generateKey({
+    curve: "ed25519",
+    userIDs: [
+      {
+        name: "Shubham Sangle",
+        email: "sangleshubham9@gmail.com",
+        comment: "This key is for public sharing",
+      },
+    ],
+    passphrase: "secret",
+  });
 
-    armoredPrivateKey = privateKey
-    armoredPublicKey = publicKey
-
+  armoredPrivateKey = privateKey;
+  armoredPublicKey = publicKey;
 }
 
-export async function encryptValue(req,res)
-{
-
-    console.log(armoredPublicKey)
-    // return
-    const publicKey = await openpgp.readKey({ armoredKey: armoredPublicKey });
-    const encrypted = await openpgp.encrypt({
-        message: await openpgp.createMessage({ text: req.body.value }), // input as Message object
-        encryptionKeys: publicKey,
-    });
-    res.status(200).send(encrypted)
+export async function encryptValue(req, res) {
+  const publicKey = await openpgp.readKey({ armoredKey: armoredPublicKey });
+  const encrypted = await openpgp.encrypt({
+    message: await openpgp.createMessage({ text: req.body.value }), // input as Message object
+    encryptionKeys: publicKey,
+  });
+  res.status(200).send(encrypted);
 }
 
+export async function decryptValue(req, res) {
+  const message = await openpgp.readMessage({
+    armoredMessage: req.body.encrypted, // parse armored message
+  });
 
-export async function decryptValue(req,res){
+  let privateKey = await openpgp.decryptKey({
+    privateKey: await openpgp.readPrivateKey({ armoredKey: armoredPrivateKey }),
+    passphrase: "secret",
+  });
 
-    // console.log(req.body)
-    const message = await openpgp.readMessage({
-        armoredMessage: req.body// parse armored message
-    });
+  const { data: decrypted, signatures } = await openpgp.decrypt({
+    message,
+    decryptionKeys: privateKey,
+  });
 
-    privateKey = await openpgp.decryptKey({
-        privateKey: await openpgp.readPrivateKey({ armoredKey: armoredPrivateKey }),
-        passphrase : "secret"
-    });
-    
-    const { data: decrypted, signatures } = await openpgp.decrypt({
-        message,
-        decryptionKeys: privateKey
-    });
-    
-
-    res.status(200).send(decrypted)
+  res.status(200).send(decrypted);
 }
